96c736703b847a368fc6f8104c142c270064ec1c
hh-harsh1 <sondagarharsh731@gmail.com>
2025-06-06 12:09:19 +0530

    added a p tag

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const page = () => {
-  return <div>hi there</div>;
+  return (
+    <div>
+      hi there
+      <p>hello there</p>
+    </div>
+  );
 };
 
 export default page;
